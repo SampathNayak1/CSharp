f44a20e68e1aa2ea5964a4cf32954945304262eb
SampathNayak1 <megavathsampath1@gmail.com>
2026-04-17 12:52:25 +0530

    Made some changes

--- CSharp/Array_Multi_dimential1.cs
+++ CSharp/Array_Multi_dimential1.cs
@@ -11,13 +11,14 @@
 //        static void Main(string[] args)
 //        {
 //            int[,] a = new int[,] { { 10, 20, 30 }, { 40, 50, 60 } };
-//            for(int i = 0; i < 2; i++)
-//            { 
-//                for(int j=0; j<3; j++)
+//            for (int i = 0; i < 2; i++)
+//            {
+//                for (int j = 0; j < 3; j++)
 //                {
 //                    Console.Write(a[i, j] + "  ");
 //                }
-//                Console.WriteLine(); 
+//                Console.WriteLine();
+//                Console.ReadLine();
 //            }
 //        }
 //    }
