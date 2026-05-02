eb0be36845a000ff63e48a5b58e1101552119bda
SampathNayak1 <megavathsampath1@gmail.com>
2026-05-02 12:22:30 +0530

    Generic Collection

---
+++ CSharp/Generic_Collection_Example1.cs
@@ -0,0 +1,45 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace CSharp
+//{
+//    internal class Generic_Collection_Example1
+//    {
+//        static void Main(string[] args)
+//        {
+//            Myclass obj = new Myclass();
+//            obj.Add<int>(100);
+//            obj.Add<String>("Sam");
+//            obj.Add<double>(1000);
+//            obj.Add1<int>(10, 20,30);
+//            obj.Add1<int>(10, 20,30);
+//            obj.Sub<decimal>(100.00m);
+//            obj.Student<int, string>(10, "Rama");
+//        }
+//    }
+
+//    class Myclass
+//    {
+//        public void Add<T>(T x)
+//        {
+//            Console.WriteLine(x);
+//        }
+//        public void Add1<T>(T a, T b ,T c)
+//        {
+                
+//            Console.WriteLine(a + "," + b);
+//        }
+//        public void Sub<T>(T a)
+//        {
+//            Console.WriteLine(a);
+//        }
+//        public void Student<T, k>(T a, k b)
+//        {
+//            Console.WriteLine(a + "," + b);
+//        }
+
+//    }
+//}
--- CSharp/Overloading_With_Differenttype_Parametrs.cs
+++ CSharp/Overloading_With_Differenttype_Parametrs.cs
@@ -1,16 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace CSharp
-{
-    internal class Overloading_With_Differenttype_Parametrs
-    {
-        static void Main(string[] args)
-        {
-
-        }
-    }
-}
+//namespace CSharp
+//{
+//    internal class Overloading_With_Differenttype_Parametrs
+//    {
+//        static void Main(string[] args)
+//        {
+//            string s1 = "Hydera";
+//            Console.WriteLine(s1.GetHashCode());
+//            s1 = s1 + "bad";
+//            Console.WriteLine(s1.GetHashCode());
+//            Console.WriteLine(s1);
+//            StringBuilder s2 = new StringBuilder("Hydera");
+//            Console.WriteLine(s2.GetHashCode());
+//            s2.Append("Bad");
+//            Console.WriteLine(s2.GetHashCode());
+//            Console.ReadLine();
+//        }
+//    }
+//}
