1348f3d0114446f9e66b2fc17e686b7e34830442
SampathNayak1 <megavathsampath1@gmail.com>
2026-05-12 12:22:49 +0530

    Student Details

--- CSharp/Method_WithRetrunType_WithParameters.cs
+++ CSharp/Method_WithRetrunType_WithParameters.cs
@@ -1,34 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace CSharp
-{
-    internal class Method_WithRetrunType_WithParameters
-    {
-        static void Main(string[] args)
-        {
-            MyClass obj= new MyClass();
-           int result= obj.Sum(10,20);
-            Console.WriteLine("result is : " +result);
-            string name = obj.Yourname("Sam");
+//namespace CSharp
+//{
+//    internal class Method_WithRetrunType_WithParameters
+//    {
+//        static void Main(string[] args)
+//        {
+//            MyClass obj= new MyClass();
+//           int result= obj.Sum(10,20);
+//            Console.WriteLine("result is : " +result);
+//            string name = obj.Yourname("Sam");
 
-            Console.WriteLine("Your name is : " +name );
-        }
-    }
-    class MyClass
-    {
-        public int Sum(int a, int b)
-        {
-            int c;
-            c = a + b;
-            return c;
-        }
-        public string Yourname(string name)
-        {
-            return name;
-        }
-    }
-}
+//            Console.WriteLine("Your name is : " +name );
+//        }
+//    }
+//    class MyClass
+//    {
+//        public int Sum(int a, int b)
+//        {
+//            int c;
+//            c = a + b;
+//            return c;
+//        }
+//        public string Yourname(string name)
+//        {
+//            return name;
+//        }
+//    }
+//}
---
+++ CSharp/StudeantDetails.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp
+{
+    class Stundent
+    {
+        static string collegename = "Narayana Junior College";
+        int Sid;
+        string Sname;
+        byte Sage;
+
+        public void StudentDetails(int Sid, string Sname, byte Sage)
+        {
+            this.Sid = Sid;
+            this.Sname = Sname;
+            this.Sage = Sage;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("Student Id is : " + Sid);
+            Console.WriteLine("Student Name is : " + Sname);
+            Console.WriteLine("Student Age is :  " + Sage);
+            Console.WriteLine("Stundent Collage is : " + collegename);
+        }
+    }
+    internal class StudeantDetails
+    {
+        static void Main(string[] args)
+        {
+            Stundent std1= new Stundent();
+            std1.StudentDetails(1,"Ram",20);
+            std1.Display(); 
+
+            std1.StudentDetails(2, "Sitha", 21);
+            std1.Display();
+
+            std1.StudentDetails(3, "Lakman", 22);
+            std1.Display();
+
+            std1.StudentDetails(4, "Hanuma", 23);
+            std1.Display();
+
+            std1.StudentDetails(5, "Karna", 24);
+            std1.Display();
+        }
+    }
+   
+}
