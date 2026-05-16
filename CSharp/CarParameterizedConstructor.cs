ba5e9edb74457108e9d0bca9088e7839d1ba2ff7
SampathNayak1 <megavathsampath1@gmail.com>
2026-05-16 09:31:51 +0530

    RealTime Connection String

---
+++ CSharp/CarParameterizedConstructor.cs
@@ -0,0 +1,54 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace CSharp
+//{
+//    class Car
+//    {
+//        string CarModel, CarColor;
+//        int CarYear;
+
+//        public Car(string carmodel, string carcolor, int caryear)
+//        {
+//            CarModel = carmodel;
+//            CarColor = carcolor;
+//            CarYear = caryear;
+
+//            if (CarYear <= 1886)
+//            {
+//                Console.WriteLine("NotFound... ");
+
+//            }
+//            else
+//            {
+//                Console.WriteLine("Car manufacturing Year is : " + CarYear);
+//            }
+
+
+
+
+//        }
+//        public void CarDetails()
+
+//        {
+
+//            Console.WriteLine("Car Model is : " + CarModel);
+//            Console.WriteLine("Car Color is : " + CarColor);
+//            Console.WriteLine("Car Year is : " + CarYear);
+//        }
+//    }
+//    internal class CarParameterizedConstructor
+//    {
+//        static void Main(string[] args)
+//        {
+//            Car car1 = new Car("BMW", "Black", 1990);
+//            car1.CarDetails();
+//            Car car2 = new Car("Benz", "White", 2025);
+//            car2.CarDetails();
+
+//        }
+//    }
+//}
--- CSharp/RealTimeConnectionStringParamerizedConstructor.cs
+++ CSharp/RealTimeConnectionStringParamerizedConstructor.cs
@@ -1,54 +1,54 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.SqlClient;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Data.SqlClient;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace CSharp
-{
-    class DataBaseConnection
-    {
-        string ConnectionString;
-        SqlConnection connection;
+//namespace CSharp
+//{
+//    class DataBaseConnection
+//    {
+//        string ConnectionString;
+//        SqlConnection connection;
 
-        public DataBaseConnection(string connString)
-        {
-            ConnectionString=connString;
-            connection=new SqlConnection(ConnectionString);
-        }
+//        public DataBaseConnection(string connString)
+//        {
+//            ConnectionString=connString;
+//            connection=new SqlConnection(ConnectionString);
+//        }
 
-        public void OpenConnection()
-        {
-            try
-            {
-                connection.Open();
-                Console.WriteLine("Database Connection Opend Succesfully");
-            }
-            catch(Exception e)
-            {
-                Console.WriteLine(e.Message);
-            }
-        }
+//        public void OpenConnection()
+//        {
+//            try
+//            {
+//                connection.Open();
+//                Console.WriteLine("Database Connection Opend Succesfully");
+//            }
+//            catch(Exception e)
+//            {
+//                Console.WriteLine(e.Message);
+//            }
+//        }
 
-        public void CloseConnection() 
-        {
-            if(connection.State == System.Data.ConnectionState.Open)
-            {
-                connection.Close();
-                Console.WriteLine("connection is closed! ");
-            }
-        }
-    } 
+//        public void CloseConnection() 
+//        {
+//            if(connection.State == System.Data.ConnectionState.Open)
+//            {
+//                connection.Close();
+//                Console.WriteLine("connection is closed! ");
+//            }
+//        }
+//    } 
 
-    internal class RealTimeConnectionStringParamerizedConstructor
-    {
-        static void Main(string[] args)
-        {
-            string connString = "Server=.;InitialCatalog=MYDB;Integrated Security=True";
-            DataBaseConnection db = new DataBaseConnection(connString);
-            db.OpenConnection();
-            db.CloseConnection();
-        }
-    }
-}
+//    internal class RealTimeConnectionStringParamerizedConstructor
+//    {
+//        static void Main(string[] args)
+//        {
+//            string connString = "Data Source=SAM\\SQLEXPRESS;Initial Catalog=Login;Integrated Security=True";
+//            DataBaseConnection db = new DataBaseConnection(connString);
+//            db.OpenConnection();
+//            db.CloseConnection();
+//        }
+//    }
+//}
