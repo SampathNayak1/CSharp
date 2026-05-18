e274fa7ec25f09037702071d7535ec2df6fdbf26
SampathNayak1 <megavathsampath1@gmail.com>
2026-05-18 12:34:46 +0530

    ProtectedAM_PassingParametersUsing_BaseKeyword

--- CSharp/InternalAM.cs
+++ CSharp/InternalAM.cs
@@ -1,43 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.ComponentModel;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace CSharp
-{
-    class MyClass
-    {
-        internal int a;
-        public MyClass()
-        {
-            a = 10;
-        }
-        public void Dispaly()
-        {
-            Console.WriteLine("a valuse is : " + a);
-        }
-    }
-    class NewClass
-    {
-        public void NewDispaly()
-        {
-            MyClass obj=new MyClass();
-            Console.WriteLine(obj.a);
-        }
-    }
-    internal class InternalAM
-    {
+//namespace CSharp
+//{
+//    class MyClass
+//    {
+//        internal int a;
+//        public MyClass()
+//        {
+//            a = 10;
+//        }
+//        public void Dispaly()
+//        {
+//            Console.WriteLine("a valuse is : " + a);
+//        }
+//    }
+//    class NewClass
+//    {
+//        public void NewDispaly()
+//        {
+//            MyClass obj=new MyClass();
+//            Console.WriteLine(obj.a);
+//        }
+//    }
+//    internal class InternalAM
+//    {
         
-        static void Main(string[] args)
-        {
-            MyClass cls1=new MyClass();
-             cls1.Dispaly();
+//        static void Main(string[] args)
+//        {
+//            MyClass cls1=new MyClass();
+//             cls1.Dispaly();
 
-            NewClass obj1=new NewClass();
-            obj1.NewDispaly();
+//            NewClass obj1=new NewClass();
+//            obj1.NewDispaly();
 
-        }
-    }
-}
+//        }
+//    }
+//}
---
+++ CSharp/ProtectedAM.cs
@@ -0,0 +1,41 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace CSharp
+//{
+//    class MyClass
+//    {
+//        protected int a ; 
+//        public void Method1()
+//        {
+//            Console.WriteLine("Method1 value is : "+a);
+//        }
+//    }
+
+//    class NewMyclass:MyClass
+//    {
+//          public NewMyclass()
+//        {
+//            a = 10;
+//        }
+        
+//        public void Method2()
+//        {
+
+//            Console.WriteLine("Method2 valuse is : "+a); 
+
+//        }
+//    } 
+//    internal class ProtectedAM
+//    {
+//        static void Main(string[] args)
+//        {
+//            NewMyclass cls1= new NewMyclass();
+//            cls1.Method1();
+//            cls1.Method2();    
+//        }
+//    }
+//}
