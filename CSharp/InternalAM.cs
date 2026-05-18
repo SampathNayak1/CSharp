e4b2a3ed0082985aa218147731a29291423a7590
SampathNayak1 <megavathsampath1@gmail.com>
2026-05-18 11:02:49 +0530

    Internal AM

---
+++ CSharp/InternalAM.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp
+{
+    class MyClass
+    {
+        internal int a;
+        public MyClass()
+        {
+            a = 10;
+        }
+        public void Dispaly()
+        {
+            Console.WriteLine("a valuse is : " + a);
+        }
+    }
+    class NewClass
+    {
+        public void NewDispaly()
+        {
+            MyClass obj=new MyClass();
+            Console.WriteLine(obj.a);
+        }
+    }
+    internal class InternalAM
+    {
+        
+        static void Main(string[] args)
+        {
+            MyClass cls1=new MyClass();
+             cls1.Dispaly();
+
+            NewClass obj1=new NewClass();
+            obj1.NewDispaly();
+
+        }
+    }
+}
